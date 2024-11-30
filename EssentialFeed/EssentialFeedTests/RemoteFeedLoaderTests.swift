3cf392897a7fff9318fd16718fe170059ef27ed2
Ricardo Calixto <38529592+RiczCalixto@users.noreply.github.com>
2024-11-30 15:58:30 -0300

    test: load request data from url (singleton approach)

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -9,16 +9,34 @@ import Foundation
 import XCTest
 
 class HTTPClient {
+  static let standard = HTTPClient()
   var requestedURL: URL?
+  private init() {}
 }
 
-struct RemoteFeedLoader {}
+struct RemoteFeedLoader {
+  func load() {
+    HTTPClient.standard.requestedURL = URL(string: "https://a-url.com")
+  }
+}
 
 class RemoteFeedLoadersTests: XCTestCase {
+  override class func tearDown() {
+    HTTPClient.standard.requestedURL = nil
+  }
+
   func test_doesNotRequestDataFromURL() {
-    let client = HTTPClient()
+    let client = HTTPClient.standard
     let _ = RemoteFeedLoader()
 
     XCTAssertNil(client.requestedURL)
+  }
+
+  func test_load_requestDataFromURL() {
+    let client = HTTPClient.standard
+    let sut = RemoteFeedLoader()
+    sut.load()
+
+    XCTAssertNotNil(client.requestedURL)
   }
 }
