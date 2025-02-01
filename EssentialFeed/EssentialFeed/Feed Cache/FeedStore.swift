afe5ec6862cec9b95b0da78839b955ac6b472abe
Ricardo Calixto <38529592+RiczCalixto@users.noreply.github.com>
2025-02-01 08:22:23 -0300

    refactor: move FeedStore and LocalFeedLoader to production code

---
+++ EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
@@ -0,0 +1,16 @@
+//
+//  FeedStore.swift
+//  EssentialFeed
+//
+//  Created by Taqtile on 01/02/25.
+//
+
+import Foundation
+
+public protocol FeedStore {
+  typealias InsertionCompletion = (Error?) -> Void
+  typealias DeletionCompletion = (Error?) -> Void
+
+  func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+  func deleteCachedFeed(completion: @escaping DeletionCompletion)
+}
